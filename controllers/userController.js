0c19e9c5ca1ce32f0bf0070474f4d7c92ab5da60
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-01-20 19:27:01 +0500

    ............

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,12 @@
+const FS = require('../lib/fsdeal');
+const users = new FS("../model/users.json")
+
 module.exports ={
   GET: (req, res) =>{
     try {
+      const allUsers = users.read();
+      console.log(allUsers);
+      
       res.render("index")
     } catch (err) {
       console.log(err);
